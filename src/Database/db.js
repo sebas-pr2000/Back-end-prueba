95186f8d13c3d1bef346ae7dcd48f43beff27113
sebastian prado <sebaslkjh@gmail.com>
2022-12-15 14:45:02 -0500

    Cambiando logica mysql2 en db.js

--- src/Database/db.js
+++ src/Database/db.js
@@ -1,11 +1,21 @@
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-   host: process.env.BD_HOST,
+const { createPool, createConnection } = require("mysql2");
+
+const connection = createConnection({
    user: process.env.DB_USER,
    password: process.env.DB_PASSWORD,
+   host: process.env.BD_HOST,
    port: process.env.DB_PORT,
    database: process.env.DB_DATABASE,
 });
+
+// const mysql = require("mysql");
+// const connection = mysql.createConnection({
+//    host: process.env.BD_HOST,
+//    user: process.env.DB_USER,
+//    password: process.env.DB_PASSWORD,
+//    port: process.env.DB_PORT,
+//    database: process.env.DB_DATABASE,
+// });
 
 connection.connect((error) => {
    if (error) {
